60794dab2d3e5f73d6d45ca0093b552c7295020a
SAKARA96 <skarandi@usc.edu>
2020-02-25 17:48:13 -0800

    PlayerStats

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{   public int health = 100;
+    public int playerBulletDamage = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
