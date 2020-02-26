4deaabc73b73716eb906641b2ec7283afdfa277d
SAKARA96 <skarandi@usc.edu>
2020-02-25 17:28:22 -0800

    EnemyStats

---
+++ Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public GameObject enemy;
+    public int health;
+    public int playerDamage;
+    public float rateofFire;
+    public string enemyTag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyTag = enemy.tag;
+
+        switch (enemyTag)
+        {
+            case "Ninja":
+                health = 100;
+                playerDamage = 50;
+                rateofFire = 1f;
+                break;
+
+            case "Ghost1":
+                health = 50;
+                playerDamage = 100;
+                rateofFire = 1f;
+                break;
+
+            case "Eagle":
+                health = 30;
+                playerDamage = 50;
+                rateofFire = 0f;
+                break;
+
+            case "Snake":
+                health = 40;
+                playerDamage = 50;
+                rateofFire = 0f;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
